88b075e7c9fce0792bb636e725307e3a89ebeaa5
zhijian <1594937473@qq.com>
2023-12-06 22:38:44 +0800

    高频

--- src/main/java/com/example/viso/controller/BarController.java
+++ src/main/java/com/example/viso/controller/BarController.java
@@ -20,8 +20,15 @@ public class BarController extends BaseController{
 
     @RequestMapping("selectBar")
     @CrossOrigin(origins = "*")
-    public JsonResult<List<BarRaceEntity>> selectBar(String category){
-        List<BarRaceEntity> list=service.selectBar(category);
+    public JsonResult<List<List>> selectBar(String category){
+        List<List> list=service.selectBar(category);
         return new JsonResult<>(OK,"查询"+category+"成功",list);
+    }
+
+    @RequestMapping("selectName")
+    @CrossOrigin(origins = "*")
+    public JsonResult<List<String>> selectName(String category){
+        List<String> names=service.selectName(category);
+        return new JsonResult<>(OK,"查询"+category+"成功",names);
     }
 }
--- src/main/java/com/example/viso/mapper/BarMapper.java
+++ src/main/java/com/example/viso/mapper/BarMapper.java
@@ -3,11 +3,13 @@ package com.example.viso.mapper;
 
 import com.example.viso.entity.BarRaceEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface BarMapper {
 
-    List<BarRaceEntity> selectBar(String category);
+    List<BarRaceEntity> selectBar(@Param("category") String category, @Param("names") List<String> names);
+    List<String> selectName(String category);
 }
